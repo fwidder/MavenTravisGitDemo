57cf6afde3b978f5a3d644df0a00d8d85f9d2002
Florian Widder <florian.widder@live.de>
2018-08-21 14:20:05 +0200

    First Version

---
+++ src/main/java/com/aut/psr5783/TravisSpringDemo/TravisSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.aut.psr5783.TravisSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisSpringDemoApplication.class, args);
+	}
+}
